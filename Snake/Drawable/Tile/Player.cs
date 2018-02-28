717cc36c27a85a41833bd166ae4175516ed29e17
kraxarn <6675728+kraxarn@users.noreply.github.com>
2018-02-28 16:18:11 +0100

    Add Player.Dead

--- Snake/Drawable/Tile/Player.cs
+++ Snake/Drawable/Tile/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Dynamic;
 using System.Windows.Forms;
 
 namespace Snake
@@ -11,16 +12,22 @@ namespace Snake
 
 		private readonly Keybinding keybinding;
 
+		private bool IsDead;
+
 		public Player(int num)
 		{
 			currentDirection = (Direction) new Random().Next(3);
 			keybinding = new Keybinding(num);
+			IsDead = false;
 		}
 
 		private void SetDirection(Direction direction) => currentDirection = direction;
 
 		public void HandleKey(Keys key)
 		{
+			if (IsDead)
+				return;
+
 			var pressed = keybinding.GetPressedDirection(key);
 			if (pressed != Keybinding.Input.None)
 				SetDirection(Keybinding.ToDirection(pressed));
@@ -31,9 +38,6 @@ namespace Snake
 			throw new NotImplementedException("Player.Draw");
 		}
 
-		public void Die()
-		{
-			throw new NotImplementedException("Player.Die");
-		}
+		public void Die() => IsDead = true;
 	}
 }

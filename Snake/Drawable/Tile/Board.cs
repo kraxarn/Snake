fb37890aebdf6d216614eb0cde49963f30157ffb
kraxarn <6675728+kraxarn@users.noreply.github.com>
2018-02-28 15:16:43 +0100

    Remove extra space

--- Snake/Drawable/Tile/Board.cs
+++ Snake/Drawable/Tile/Board.cs
@@ -21,7 +21,7 @@ namespace Snake
 					tiles[x, y].Draw(g);
 		}
 
-		public void  FillWithRandomTiles()
+		public void FillWithRandomTiles()
 		{
 			for (var x = 0; x < tiles.GetLength(0); x++)
 				for (var y = 0; y < tiles.GetLength(1); y++)

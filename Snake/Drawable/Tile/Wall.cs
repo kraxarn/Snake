5b027e4867bd641b1c2055891ac613504dc72cb9
kraxarn <emil202821@hotmail.com>
2018-03-04 22:27:09 +0100

    Implement Wall

--- Snake/Drawable/Tile/Wall.cs
+++ Snake/Drawable/Tile/Wall.cs
@@ -1,7 +1,11 @@
-﻿namespace Snake
+﻿using System.Drawing;
+
+namespace Snake
 {
 	public class Wall : Tile
 	{
+		public Wall() => SetColor(Color.FromArgb(255, 193, 7));
+
 		public override bool Collide(Player player)
 		{
 			player.Die();

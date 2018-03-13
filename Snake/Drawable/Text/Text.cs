a7f6a0a28d7949ac7c47acb5186cfdf71e3f9217
kraxarn <emil202821@hotmail.com>
2018-03-14 00:42:19 +0100

    Add StringFormat to Text and add Text.CenterLabel()

--- Snake/Drawable/Text/Text.cs
+++ Snake/Drawable/Text/Text.cs
@@ -6,6 +6,7 @@ namespace Snake
 	{
 		private readonly Font drawFont;
 		private readonly SolidBrush drawer;
+		private readonly StringFormat format;
 
 		private Point pos;
 
@@ -14,12 +15,19 @@ namespace Snake
 		public Text(string label, string font, int size, Point pos, Color color)
 		{
 			drawFont = new Font(font, size);
-			drawer = new SolidBrush(color);
+			drawer   = new SolidBrush(color);
+			format   = new StringFormat();
 
-			Label = label;
+			Label    = label;
 			this.pos = pos;
 		}
 
-		public void Draw(Graphics g, int x = 0, int y = 0) => g.DrawString(Label, drawFont, drawer, pos.X, pos.Y);
+		public void Draw(Graphics g, int x = 0, int y = 0) => g.DrawString(Label, drawFont, drawer, pos.X, pos.Y, format);
+
+		public void CenterLabel()
+		{
+			format.Alignment     = StringAlignment.Center;
+			format.LineAlignment = StringAlignment.Center;
+		}
 	}
 }

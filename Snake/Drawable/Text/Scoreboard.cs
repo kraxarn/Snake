56271e7d382d5dc0a8db45e902aa21215e7bc89a
kraxarn <emil202821@hotmail.com>
2018-03-18 20:55:05 +0100

    Add description to methods in Scoreboard

--- Snake/Drawable/Text/Scoreboard.cs
+++ Snake/Drawable/Text/Scoreboard.cs
@@ -12,20 +12,19 @@ namespace Snake
 {
 	public class Scoreboard : IDrawable
 	{
+		// The actual text being drawn to the scren
 		private readonly Text text;
 
+		// The score
 		public int Score { get; private set; }
 
-		public Scoreboard(int player, Color color, Point position)
-		{
-			text = new Text($"P{player}: 0", "Consolas", 12, position, color);
-		}
+		// Constructor to create the text
+		public Scoreboard(int player, Color color, Point position) => text = new Text($"P{player}: 0", "Consolas", 12, position, color);
 
-		public void Draw(Graphics g, int x = 0, int y = 0)
-		{
-			text.Draw(g, x, y);
-		}
+		// Draw the text for the scoreboard
+		public void Draw(Graphics g, int x = 0, int y = 0) => text.Draw(g, x, y);
 
+		// When we change score, we also want to update the label
 		public void ChangeScore(int amount)
 		{
 			Score += amount;

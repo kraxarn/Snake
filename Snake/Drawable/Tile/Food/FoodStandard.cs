cff9638c325c4a71ea9a2d084691acf766ab549f
kraxarn <emil202821@hotmail.com>
2018-03-18 21:07:25 +0100

    Add FoodStandard description

--- Snake/Drawable/Tile/Food/FoodStandard.cs
+++ Snake/Drawable/Tile/Food/FoodStandard.cs
@@ -1,4 +1,14 @@
-﻿namespace Snake
+﻿
+/*
+ * FoodStandard
+ *
+ * A type of food that
+ * grows the snake by 1
+ * tile and awards them
+ * with 1 point.
+ */
+
+namespace Snake
 {
 	public class FoodStandard : Food
 	{

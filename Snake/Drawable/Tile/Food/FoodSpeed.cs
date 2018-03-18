5d3781010301483998ff47ae2de01ad68584f1bd
kraxarn <emil202821@hotmail.com>
2018-03-18 21:06:35 +0100

    Add FoodSpeed description

--- Snake/Drawable/Tile/Food/FoodSpeed.cs
+++ Snake/Drawable/Tile/Food/FoodSpeed.cs
@@ -1,4 +1,17 @@
-﻿namespace Snake
+﻿
+/*
+ * FoodSpeed
+ *
+ * A type of food that
+ * randomly speeds up a
+ * player on the board.
+ * Since we can't manage all
+ * players directly from here,
+ * we have to keep a reference to
+ * the main Engine class.
+ */
+
+namespace Snake
 {
 	public class FoodSpeed : Food
 	{

0a944d8e46e5df7b5b969f0109b73b429d3a9028
kraxarn <emil202821@hotmail.com>
2018-03-18 21:20:04 +0100

    Add Program description

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 
+/*
+ * Program
+ *
+ * Default class to act
+ * as the main entry point for
+ * the application.
+ */
+
 namespace Snake
 {
 	internal static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		private static void Main()
 		{
+			// Check how many players we need
 			var players = new FormPlayers();
 			players.ShowDialog();
 			var numPlayers = players.GetNumPlayers();
 			if (numPlayers == 0)
 				return;
-
-
+			
+			// Create the main engine with desired amount of players
 			var engine = new Engine(numPlayers);
 			engine.Run();
 		}

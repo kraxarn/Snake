e3c590080517149570c5de2d02f259866153f9fa
kraxarn <emil202821@hotmail.com>
2018-03-18 21:17:39 +0100

    Add IDrawable description

--- Snake/Interface/IDrawable.cs
+++ Snake/Interface/IDrawable.cs
@@ -1,5 +1,14 @@
 ﻿using System.Drawing;
 
+/*
+ * IDrawable
+ *
+ * Interface that tells the
+ * class can be drawn to the screen.
+ * This is mainly used for tiles, but
+ * also used for text.
+ */
+
 namespace Snake
 {
 	public interface IDrawable

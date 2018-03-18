863063433074248bc9d75a275e4f7e936a8833dc
kraxarn <emil202821@hotmail.com>
2018-03-18 21:04:29 +0100

    Add Food description

--- Snake/Drawable/Tile/Food/Food.cs
+++ Snake/Drawable/Tile/Food/Food.cs
@@ -1,5 +1,15 @@
 ﻿using System.Drawing;
 
+/*
+ * Food
+ *
+ * A type of tile that's
+ * the base for different
+ * types of food. It also
+ * makes sure all foods have
+ * the same color.
+ */
+
 namespace Snake
 {
 	public abstract class Food : Tile

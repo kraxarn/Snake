cdfb489a37bdbd0917fdf78f175e461e5c107ccf
kraxarn <emil202821@hotmail.com>
2018-03-18 20:57:22 +0100

    Add description to Text

--- Snake/Drawable/Text/Text.cs
+++ Snake/Drawable/Text/Text.cs
@@ -1,16 +1,26 @@
 ﻿using System.Drawing;
 
+/*
+ * Text
+ *
+ * Class to easier draw text to the
+ * screen without having to create
+ * seperate fonts, brushes etc.
+ */
+
 namespace Snake
 {
 	public class Text : IDrawable
 	{
+		// Font being used for the text
 		private readonly Font drawFont;
 		private readonly SolidBrush drawer;
+		// String format so we can align the text
 		private readonly StringFormat format;
 
-		private Point pos;
-
-		public string Label;
+		// Position to draw the text
+		private Point  pos;
+		public  string Label;
 
 		public Text(string label, string font, int size, Point pos, Color color)
 		{
@@ -24,6 +34,8 @@ namespace Snake
 
 		public void Draw(Graphics g, int x = 0, int y = 0) => g.DrawString(Label, drawFont, drawer, pos.X, pos.Y, format);
 
+		// Sets the point where we draw it to the center of the text
+		// By default, it's set to the top left
 		public void CenterLabel()
 		{
 			format.Alignment     = StringAlignment.Center;
